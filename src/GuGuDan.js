074599d936ab7c558c539e3b5f40b685f8a0c7c5
SeeYoung Jin <jsee53@naver.com>
2023-02-23 17:49:17 +0900

    2-23

--- src/GuGuDan.js
+++ src/GuGuDan.js
@@ -2,26 +2,42 @@ import { useState } from "react";
 import "./GuGuDan.css";
 
 function GuGuDan() {
-  const [first, SetFirstNumber] = useState(Math.ceil(Math.random() * 9));
-  const [second, SetSecondNumber] = useState(Math.ceil(Math.random() * 9));
-  const [result, SetResult] = useState(false);
+  const [first, SetFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, SetSecond] = useState(Math.ceil(Math.random() * 9));
+  const [answer, SetAnswer] = useState();
+  const [submitAnswer, SetSubmitAnswer] = useState(false);
+
+  const ansCheck = (e) => {
+    SetAnswer(e.target.value);
+    SetSubmitAnswer(false);
+  };
+
+  const showResult = (e) => {
+    return (
+      <div>
+        {first * second == answer ? (
+          <div>정답입니다!</div>
+        ) : (
+          <div>틀렸습니다!</div>
+        )}
+      </div>
+    );
+  };
   return (
     <div className="GuGuDan">
       <div>
         <div>
-          {result ? (
-            <div>true</div>
-          ) : (
-            <div>
-              {first} x {second} = ?
-            </div>
-          )}
+          {first} x {second} = ?
         </div>
-        <form action="">
-          <input type="number" placeholder="정답 입력:" />
-          <button>제출</button>
-        </form>
-        <div>{result ? <div>정답입니다!</div> : <div>틀렸습니다!</div>}</div>
+        <input type="number" onChange={ansCheck} placeholder="정답 입력:" />
+        <button
+          onClick={() => {
+            SetSubmitAnswer(true);
+          }}
+        >
+          제출
+        </button>
+        {submitAnswer ? showResult() : null}
       </div>
     </div>
   );
--- src/Number_Baseball.js
+++ src/Number_Baseball.js
@@ -2,9 +2,86 @@ import { useState } from "react";
 import "./Number_Baseball.css";
 
 function Number_Baseball() {
+  const [answer, SetAnswer] = useState();
+
+  const ansCheck = (e) => {
+    SetAnswer(e.target.value);
+  };
+
   return (
     <div className="Number_Baseball">
-      <div>숫자야구</div>
+      <div class="title">
+        <span class="gradtext">숫자야구</span>
+        <table class="gameTable" id="gameTable">
+          <tr>
+            <td colspan="3">SCORE</td>
+          </tr>
+          <tr>
+            <td>1회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>2회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>3회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>4회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>5회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>6회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>7회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>8회</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>9회</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="input">
+        <h4>서로 다른 네 자리 숫자를 입력하세요. (0은 제외)</h4>
+        <form id="form">
+          <input
+            class="text"
+            id="inputNumber"
+            onChange={ansCheck}
+            type="number"
+            placeholder="ex) 1234"
+            onfocus="this.placeholder=''"
+            onblur="this.placeholder='ex) 1234'"
+          />
+          <span class="gameButton">
+            <input class="resultButton" type="submit" value="정답입력" />
+          </span>
+        </form>
+      </div>
+      {answer}
     </div>
   );
 }
